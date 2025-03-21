1f27bf53103b2361911040250945b5d0404c90f1
Kummirai <ajaxmilton@hotmail.com>
2025-03-21 17:26:07 +0200

    Add country dat from Api

--- scripts/getWeather.js
+++ scripts/getWeather.js
@@ -1,13 +1,27 @@
-const getWeather = async () =>{
-    try{
-        const response = await fetch("https://api.openweathermap.org/data/3.0/onecall?lat=30.5595°&lon=22.9375°&appid=9EzdJ3lhcAeVxFKP");
-        const data = await response.json;
-        console.log(data);
-    } catch (error){
-        console.log("Error fetching data", error);
+const countryDataCard = document.querySelector(".country-data");
+let countryData = "";
+
+const getCountry = () => {
+    fetch("https://restcountries.com/v3.1/name/south%20africa")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data[0])
+            const countryInfo = data[0];
+            countryData = `
+                        <p><span>Area: </span>${countryInfo.area}</p>
+                        <p><span>Population: </span>${countryInfo.population}</p>
+                        <p><span>Capital: </span>${countryInfo.capital}</p>
+                        <p><span>Languages: </span>${Object.values(countryInfo.languages)}</p>
+                        <p><span>Currency: </span>$</p>
+                        <p><span>Timezone: </span>${countryInfo.timezones}</p>
+                        <p><span>Calling Code: </span>+27</p>
+                        <p><span>Internet: </span>.mg</p>
+        `;
+        console.log(countryData);
         
-    }
-}
+        countryDataCard.innerHTML = countryData;
+}) 
+        .catch(error => console.error("Error fetching data:", error));
+};
 
-
-getWeather();
+getCountry();
