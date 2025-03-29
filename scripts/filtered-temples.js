a14347a9654c67c2a27341a0c6456a6f1f37f627
Kummirai <ajaxmilton@hotmail.com>
2025-03-29 12:49:58 +0200

    Add additional temple details and improve styling for filtered temples page

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -55,6 +55,31 @@ const temples = [
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
     },
+    {
+        templeName: "San Diego California",
+        location: "San Diego, California, United States",
+        dedicated: "1993, April, 25",
+        area: 72000,
+        imageUrl:
+        "https://churchofjesuschristtemples.org/assets/img/temples/san-diego-california-temple/san-diego-california-temple-9060-main.jpg"
+        },
+        {
+        templeName: "Salt Lake City Utah",
+        location: "Salt Lake City, Utah, United States",
+        dedicated: "1893, April, 5",
+        area: 382207,
+        imageUrl:
+        "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-15669-main.jpg"
+    },
+    {
+        templeName: "Los Angeles California",
+        location: "Los Angeles, California, United States",
+        dedicated: "1937, April, 13",
+        area: 189000,
+        imageUrl:
+        "https://churchofjesuschristtemples.org/assets/img/temples/los-angeles-california-temple/los-angeles-california-temple-38945-main.jpg"
+    }
+
     // Add more temple objects here...
   ];
 
@@ -73,7 +98,6 @@ const temples = [
                     </figure>
                 </div>
     `;
-    console.log(cards);
     cardContainer.innerHTML = cards;
   })
 
