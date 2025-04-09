a4e1febb4400c630aeb210b1f0d2bb0abfd35096
Kummirai <ajaxmilton@hotmail.com>
2025-04-09 23:07:08 +0200

    Add footer section and update site plan with dynamic date information

---
+++ project/scripts/getdate.js
@@ -0,0 +1,8 @@
+const currentYear =  document.querySelector("#currentyear");
+const lastModified =  document.querySelector("#lastModified");
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+currentYear.innerHTML = year;
+lastModified.innerHTML = document.lastModified;
